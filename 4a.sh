fb978c28beacc51799092d5c381cedb2df6f901f
tuuuuuuuuuuuuuuuuuuuu <108440953+tuuuuuuuuuuuuuuuuuuuu@users.noreply.github.com>
2022-07-05 15:46:58 +0700

    Update 4a.sh

--- 4a.sh
+++ 4a.sh
@@ -1,30 +1,27 @@
-#!/bin/sh
-num=0
-date
-set `date`
-while [ $num -eq 0 ]
-	do 
-		echo "Nhap vao mot file:"
-		read so
-		if test -e "$so"
+#!/bin/bash
+count=0
+while [ $count -eq 0 ] 
+	do
+		echo "Nhap vao ten file: "
+		read namefile
+		if [ -e $namefile ]
 			then
-				echo "file da ton tai"
-					if test -f "$so"
-						then 
-							echo "Day la file thong thuong"
-					else
-						echo "Day khong la file thong thuong"
-					fi
+				echo "$namefile co ton tai! "
+				if [ -f $namefile ]
+					then
+						echo "Va la file thong thuong"
+				else
+					echo "Va khong phai la file thong thuong"
+				fi
 		else
-			echo "file khong ton tai"
+			echo "$namefile khong ton tai"
 		fi
-		echo -e "Hom nay la thu $1 ngay $3 thang $2 nam $6\nGio hien nay $4"
-		echo "Ban co muon tiep tuc? y/n ?"
+		echo "Tiep tuc chon 1, thoat chon 0"
 		read choice
-		if [ "$choice" = "y" ];
-			then
-			num=0
-		else
-			num=1
+		if [ $choice -eq 1 ]
+			then 
+				count=0
+		else 
+			count=1
 		fi
-	done
+done
